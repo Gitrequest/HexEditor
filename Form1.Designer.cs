7edcd6ee4806ac25da893bc9da89ade0e063bae6
Xiras <benny.lueffe@gmail.com>
2021-10-21 15:32:31 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace HexKonverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl_navbar = new System.Windows.Forms.Panel();
+            this.pnl_nav = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn_HexToText = new System.Windows.Forms.Button();
+            this.btn_texttohex = new System.Windows.Forms.Button();
+            this.pnl_logo = new System.Windows.Forms.Panel();
+            this.label_title = new System.Windows.Forms.Label();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.pnl_navbar.SuspendLayout();
+            this.pnl_logo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_navbar
+            // 
+            this.pnl_navbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.pnl_navbar.Controls.Add(this.pnl_nav);
+            this.pnl_navbar.Controls.Add(this.button8);
+            this.pnl_navbar.Controls.Add(this.button7);
+            this.pnl_navbar.Controls.Add(this.button6);
+            this.pnl_navbar.Controls.Add(this.button5);
+            this.pnl_navbar.Controls.Add(this.button4);
+            this.pnl_navbar.Controls.Add(this.btn_HexToText);
+            this.pnl_navbar.Controls.Add(this.btn_texttohex);
+            this.pnl_navbar.Controls.Add(this.pnl_logo);
+            this.pnl_navbar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_navbar.Location = new System.Drawing.Point(0, 0);
+            this.pnl_navbar.Name = "pnl_navbar";
+            this.pnl_navbar.Size = new System.Drawing.Size(200, 881);
+            this.pnl_navbar.TabIndex = 0;
+            // 
+            // pnl_nav
+            // 
+            this.pnl_nav.BackColor = System.Drawing.Color.Coral;
+            this.pnl_nav.Location = new System.Drawing.Point(3, 268);
+            this.pnl_nav.Name = "pnl_nav";
+            this.pnl_nav.Size = new System.Drawing.Size(4, 150);
+            this.pnl_nav.TabIndex = 1;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button8.ForeColor = System.Drawing.Color.Coral;
+            this.button8.Location = new System.Drawing.Point(0, 412);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(200, 53);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Visible = false;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button7.ForeColor = System.Drawing.Color.Coral;
+            this.button7.Location = new System.Drawing.Point(0, 359);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(200, 53);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.ForeColor = System.Drawing.Color.Coral;
+            this.button6.Location = new System.Drawing.Point(0, 306);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(200, 53);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.Color.Coral;
+            this.button5.Location = new System.Drawing.Point(0, 253);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 53);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.Coral;
+            this.button4.Location = new System.Drawing.Point(0, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 53);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // btn_HexToText
+            // 
+            this.btn_HexToText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_HexToText.FlatAppearance.BorderSize = 0;
+            this.btn_HexToText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_HexToText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_HexToText.ForeColor = System.Drawing.Color.Coral;
+            this.btn_HexToText.Location = new System.Drawing.Point(0, 147);
+            this.btn_HexToText.Name = "btn_HexToText";
+            this.btn_HexToText.Size = new System.Drawing.Size(200, 53);
+            this.btn_HexToText.TabIndex = 3;
+            this.btn_HexToText.Text = "Hex to Text";
+            this.btn_HexToText.UseVisualStyleBackColor = true;
+            this.btn_HexToText.Click += new System.EventHandler(this.btn_HexToText_Click);
+            this.btn_HexToText.Leave += new System.EventHandler(this.btn_HexToText_Leave);
+            // 
+            // btn_texttohex
+            // 
+            this.btn_texttohex.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_texttohex.FlatAppearance.BorderSize = 0;
+            this.btn_texttohex.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_texttohex.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_texttohex.ForeColor = System.Drawing.Color.Coral;
+            this.btn_texttohex.Location = new System.Drawing.Point(0, 94);
+            this.btn_texttohex.Name = "btn_texttohex";
+            this.btn_texttohex.Size = new System.Drawing.Size(200, 53);
+            this.btn_texttohex.TabIndex = 1;
+            this.btn_texttohex.Text = "Text to Hex";
+            this.btn_texttohex.UseVisualStyleBackColor = true;
+            this.btn_texttohex.Click += new System.EventHandler(this.btn_texttohex_Click);
+            this.btn_texttohex.DragLeave += new System.EventHandler(this.btn_texttohex_DragLeave);
+            this.btn_texttohex.Leave += new System.EventHandler(this.btn_texttohex_Leave);
+            // 
+            // pnl_logo
+            // 
+            this.pnl_logo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(40)))), ((int)(((byte)(54)))));
+            this.pnl_logo.Controls.Add(this.label_title);
+            this.pnl_logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_logo.Location = new System.Drawing.Point(0, 0);
+            this.pnl_logo.Name = "pnl_logo";
+            this.pnl_logo.Size = new System.Drawing.Size(200, 94);
+            this.pnl_logo.TabIndex = 0;
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_title.ForeColor = System.Drawing.Color.Coral;
+            this.label_title.Location = new System.Drawing.Point(12, 37);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(163, 31);
+            this.label_title.TabIndex = 1;
+            this.label_title.Text = "Text to Hex";
+            // 
+            // txt_input
+            // 
+            this.txt_input.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_input.ForeColor = System.Drawing.Color.Coral;
+            this.txt_input.Location = new System.Drawing.Point(206, 94);
+            this.txt_input.Multiline = true;
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(966, 380);
+            this.txt_input.TabIndex = 1;
+            this.txt_input.TextChanged += new System.EventHandler(this.txt_input_TextChanged);
+            // 
+            // txt_output
+            // 
+            this.txt_output.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_output.ForeColor = System.Drawing.Color.GreenYellow;
+            this.txt_output.Location = new System.Drawing.Point(206, 489);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ReadOnly = true;
+            this.txt_output.Size = new System.Drawing.Size(966, 380);
+            this.txt_output.TabIndex = 2;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_convert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_convert.ForeColor = System.Drawing.Color.Coral;
+            this.btn_convert.Location = new System.Drawing.Point(206, 33);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(156, 48);
+            this.btn_convert.TabIndex = 9;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1184, 881);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.pnl_navbar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HexEditor";
+            this.pnl_navbar.ResumeLayout(false);
+            this.pnl_logo.ResumeLayout(false);
+            this.pnl_logo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_navbar;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_HexToText;
+        private System.Windows.Forms.Button btn_texttohex;
+        private System.Windows.Forms.Panel pnl_logo;
+        private System.Windows.Forms.Panel pnl_nav;
+        private System.Windows.Forms.Label label_title;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.TextBox txt_output;
+        private System.Windows.Forms.Button btn_convert;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HexKonverter
+{
+
+
+
+
+
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            Global.Flag = "1";
+
+
+
+            InitializeComponent();
+
+
+            // Awesome looking Panel to the left as button marker
+            pnl_nav.Height = btn_texttohex.Height;
+            pnl_nav.Top = btn_texttohex.Top;
+            pnl_nav.Left = btn_texttohex.Left;
+
+            //Changing label -> User can see what exactly now happens
+            label_title.Text = "Text to Hex";
+
+            //Colorize the world, background button color for idiot proof readability
+            btn_texttohex.BackColor = Color.FromArgb(46, 51, 73);
+
+
+
+
+        }
+
+
+
+        public class Global
+        {
+            //public static int flag;
+            public static string Flag { get; set; }
+        }
+
+
+        private string RemoveSpecialCharacters(string str)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str[i] >= '0' && str[i] <= '9')
+                    || (str[i] >= 'A' && str[i] <= 'z'
+                        || (str[i] == '.' || str[i] == '_')))
+                {
+                    sb.Append(str[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+
+        private void TextToHex()
+        {
+
+            if (String.IsNullOrEmpty(txt_input.Text))
+            {
+                
+                txt_output.Text = "Please enter something before trying to convert!";
+            }
+            else
+            {
+
+                string result = "";
+                string toConvert = txt_input.Text;
+                char[] values = toConvert.ToCharArray();
+                foreach (char letter in values)
+                {
+                    
+                    int value = Convert.ToInt32(letter);
+                    
+                    //Console.WriteLine($"Hexadecimal value of {letter} is {value:X}");
+                    
+                    result += ($"{value:X}");
+
+                    
+                }
+
+                result = Regex.Replace(result, ".{2}", "$0 ");
+                txt_output.Text = result;
+
+            }
+        }
+
+
+        private void HexToText()
+        {
+
+
+            // Check if box is empty / Not really nescessary tho
+            if (String.IsNullOrEmpty(txt_input.Text))
+            {
+
+                txt_output.Text = "Please enter something before trying to convert!";
+            }
+            else
+            {
+                string result = "";
+                string toConvert = txt_input.Text;
+
+
+                // Deciding if its plausible hexcode
+                List<string> listHexValidation = new List<string>();
+                listHexValidation.Add("g");
+                listHexValidation.Add("h");
+                listHexValidation.Add("i");
+                listHexValidation.Add("j");
+                listHexValidation.Add("k");
+                listHexValidation.Add("l");
+                listHexValidation.Add("m");
+                listHexValidation.Add("n");
+                listHexValidation.Add("o");
+                listHexValidation.Add("p");
+                listHexValidation.Add("q");
+                listHexValidation.Add("r");
+                listHexValidation.Add("s");
+                listHexValidation.Add("t");
+                listHexValidation.Add("u");
+                listHexValidation.Add("v");              
+                listHexValidation.Add("w");
+                listHexValidation.Add("x");
+                listHexValidation.Add("y");
+                listHexValidation.Add("z");
+
+                // If "HexCode" contains any illegal hex chars, it will give error out.
+                if (listHexValidation.Any(s => toConvert.Contains(s)))
+                {
+                    txt_output.Text = "No valid Hex Code!";
+
+
+
+                }
+
+                else
+                {
+                    // Cleaning up any special chars in string then removing all spaces and finally (---make space after every 2 char for hex convert/ one liner does that)
+                    
+                    try
+                    {
+                        toConvert = RemoveSpecialCharacters(toConvert);
+                        toConvert = Regex.Replace(toConvert, @"\s+", "");
+                        //changed code below from foreach to .NET5 convert due to out of range exceptions 
+                        //toConvert = Regex.Replace(toConvert, ".{2}", "$0 ");
+                        result = Encoding.UTF8.GetString(Convert.FromHexString(toConvert));
+                        txt_output.Text = result;
+                    }
+
+                    catch (Exception)
+                    {
+                        //Mostly getting not dividable /2 exception due to NO VALID HEXCODE -
+                        txt_output.Text = "Unknown Error or no valid Hex Code!\n Please check if your input is really hexcode.";
+                    }
+
+                    
+
+
+                    
+
+
+
+                }
+            }
+        }
+
+
+        //UI / Button
+        private void txt_input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_HexToText_Click(object sender, EventArgs e)
+        {
+            // I love global variables, OOP..... NAH. - Sets a flag for the converter -> No need for formloader then
+            Global.Flag = "2";
+
+            //Delete artifacts from input/ouput box
+            txt_input.Clear();
+            txt_output.Clear();
+            // Awesome looking Panel to the left as button marker
+            pnl_nav.Height = btn_HexToText.Height;
+            pnl_nav.Top = btn_HexToText.Top;
+            pnl_nav.Left = btn_HexToText.Left;
+
+            //Changing label -> User can see what exactly now happens
+            label_title.Text = "Hex to Text";
+
+            //Colorize the world, background button color for idiot proof readability
+            btn_HexToText.BackColor = Color.FromArgb(46, 51, 73);
+
+        }
+
+        private void btn_texttohex_Click(object sender, EventArgs e)
+        {
+            // I love global variables, OOP..... NAH. - Sets a flag for the converter -> No need for formloader then
+            Global.Flag = "1";
+
+            txt_input.Clear();
+            txt_output.Clear();
+
+            // Awesome looking Panel to the left as button marker
+            pnl_nav.Height = btn_texttohex.Height;
+            pnl_nav.Top = btn_texttohex.Top;
+            pnl_nav.Left = btn_texttohex.Left;
+
+            //Changing label -> User can see what exactly now happens
+            label_title.Text = "Text to Hex";
+
+            //Colorize the world, background button color for idiot proof readability
+            btn_texttohex.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+
+            string flag = Global.Flag; 
+
+
+            if (flag == "1")
+            {
+                TextToHex();
+
+            }
+
+            else if (flag == "2")
+            {
+
+                HexToText();
+            }
+
+            else if (flag == "3")
+            {
+                txt_input.Text = "ERROR FLAG 3";
+            }
+            else
+            {
+                txt_input.Text = "ERROR";
+            }
+            
+        }
+
+        private void btn_texttohex_DragLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_texttohex_Leave(object sender, EventArgs e)
+        {
+            btn_texttohex.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btn_HexToText_Leave(object sender, EventArgs e)
+        {
+            btn_HexToText.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+
+
+
+
+    }
+}
